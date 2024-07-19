c19d646fe5919bcdec95119879029df0b504aa86
aozorarb <aozorarb@icloud.com>
2024-07-20 00:20:39 +0900

    initialize

---
+++ lib/rough_diary.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rough_diary/version"
+
+module RoughDiary
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_rough_diary.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestRoughDiary < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::RoughDiary::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
