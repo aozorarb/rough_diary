0e7b10cff2d745583767c6cb51dc5e8dc00ac41e
aozorarb <aozorarb@icloud.com>
2024-08-01 01:30:17 +0900

    test: Config::NEED_FOLLOW_DIARY_TYPE

---
+++ test/test_config.rb
@@ -0,0 +1,15 @@
+require_relative 'test_helper'
+
+class RoughDiary::Config::Test < Minitest::Test
+  include RoughDiary::Config
+  def test_need_follow_diary_type
+    res =
+      NEED_FOLLOW_DIARY_TYPE.all? do |need_type_pair|
+        need_type = need_type_pair[0]
+        VALID_DIARY_TYPE.include?(need_type)
+      end
+
+    assert(res)
+  end
+
+end
