42ed0cd658f267b6f9476d5f2fd1b58767646d70
aozorarb <aozorarb@icloud.com>
2024-08-01 15:51:28 +0900

    test: check config consts are not nil

--- test/test_config.rb
+++ test/test_config.rb
@@ -2,6 +2,14 @@ require_relative 'test_helper'
 
 class RoughDiary::Config::Test < Minitest::Test
   include RoughDiary::Config
+
+  def test_constants_not_nil
+    RoughDiary::Config.constants.all? do |const|
+      !const.nil?
+    end
+  end
+
+
   def test_need_follow_diary_type
     res =
       NEED_FOLLOW_DIARY_TYPE.all? do |need_type_pair|
