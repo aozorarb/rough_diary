7ee5afc5808fbeeb5dd6a3a15f3a92b8fa5d9b31
aozorarb <aozorarb@icloud.com>
2024-08-08 23:42:01 +0900

    test: DiaryContentGenerator

--- test/test_diary_content_generator.rb
+++ test/test_diary_content_generator.rb
@@ -3,6 +3,42 @@
 require "test_helper"
 
 class TestDiaryContentGenerator < Minitest::Test
-  def test_run
+  def setup
+    @mock_savedata_manager = Minitest::Mock.new
+    @mock_tempfile = Minitest::Mock.new
+
+    Tempfile.stub :create, @mock_tempfile do
+      @generator = RoughDiary::DiaryContentGenerator.new(@mock_savedata_manager)
+    end
+
   end
+
+
+  def test_run
+    @mock_tempfile.expect :read, 'mock diary content'
+    @mock_savedata_manager.expect :content_data=, nil, ['mock diary content']
+
+    @generator.stub :edit_tempfile, nil do
+      @generator.run
+    end
+
+    @mock_tempfile.verify
+    @mock_savedata_manager.verify
+  end
+
+
+  def test_valid_editor?
+    assert @generator.send(:valid_editor?, 'vim')
+    refute @generator.send(:valid_editor?, 'invalid_editor')
+  end
+
+
+  def test_edit_tempfile_raise_error_for_invalid_editor
+    @generator.stub :valid_editor?, false do
+      assert_raises(RoughDiary::InvalidConfigrationError) do
+        @generator.send(:edit_tempfile, editor: 'invalid_editor')
+      end
+    end
+  end
+
 end
