5d57cbefa45a6b5746e11f4c0a9acb00d2ee8a11
aozorarb <aozorarb@icloud.com>
2024-08-11 15:44:49 +0900

    chore: fix about bin/consele

--- bin/helper/test_writer.rb
+++ bin/helper/test_writer.rb
@@ -1,7 +1,7 @@
 include RoughDiary
 
 def setup_helper
-  savedata = SavedataManager.new
+  savedata = SavedataManager.new('resource/article')
   database = DatabaseManager.new('test.db')
   database.savedata_manager = savedata
   writer = Writer.new(database)
