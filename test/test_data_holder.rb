75e169019eb241038808286745a0020510e78fe2
aozorarb <aozorarb@icloud.com>
2024-08-16 21:21:41 +0900

    test: DataHolder

---
+++ test/test_data_holder.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require_relative 'helper/config_define'
+
+include RoughDiary
+$create_date_regexp = /.{2}-.{2}-.{2} .{2}:.{2}:.{2}/
+
+class DataHolder::Normal::Test < Minitest::Test
+  def setup
+    @holder = DataHolder::Normal.new
+  end
+
+
+  def test_initialize
+    assert_nil @holder.get(:id)
+    assert_instance_of Time, @holder.get(:create_date)
+    assert_equal Config::DEFAULT_DIARY_TITLE, @holder.get(:title)
+    assert_nil @holder.get(:content)
+  end
+
+
+  def test_database_format
+    @holder.data_content = 'test'
+
+    data = @holder.database_format
+    assert_match $create_date_regexp, data.create_date
+    assert_equal Config::DEFAULT_DIARY_TITLE, data.title
+    assert_equal 'test', data.content
+  end
+
+end
+
+
+class DataHolder::Fix::Test < Minitest::Test
+  def setup
+    @holder = DataHolder::Fix.new(1)
+  end
+
+
+  def test_initialize
+    assert_nil @holder.get(:id)
+    assert_instance_of Time, @holder.get(:create_date)
+    assert_equal 1, @holder.get(:fix_diary_id)
+    assert_nil @holder.get(:edit_content)
+  end
+
+
+  def test_database_format
+    # TODO: rewrite edit_content as actual data format(yaml)
+    @holder.data_edit_content = 'test example'
+
+    data = @holder.database_format
+    assert_match $create_date_regexp, data.create_date
+    assert_equal 1, data.fix_diary_id
+    assert_equal 'test example', data.edit_content
+  end
+end
+
+
