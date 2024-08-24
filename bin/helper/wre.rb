dd04640bf07215124f7e15411e7b15287621f0a2
aozorarb <aozorarb@icloud.com>
2024-08-25 01:24:56 +0900

    chore: bin/console helper support editor

--- bin/helper/wre.rb
+++ bin/helper/wre.rb
@@ -6,8 +6,23 @@ def setup_helper
   database.data_holder = data_holder
   database.manager = DatabaseManager::Normal
 
+  switch_normal = proc { database.manager = DatabaseManager::Normal }
+  switch_fix    = proc { database_manager = DatabaseManager::Fix }
+
   db = database.instance_variable_get(:@manager).instance_variable_get(:@database)
+
   writer = Writer.new(database, data_holder: data_holder)
+
+  editor = nil
+  edit_data_holder = nil
+  create_editor = lambda do |follow_id|
+    switch_fix.call
+    edit_data_holder = DataHolder::Fix.new(follow_id)
+    editor = Editor.new(database, follow_id, data_holder: edit_data_holder)
+  end
+
+
+
   binding
 end
 
