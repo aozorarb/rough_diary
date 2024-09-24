f28b47c9f959ef69e653f509686e7da7e509dc28
aozorarb <aozorarb@icloud.com>
2024-09-25 01:15:48 +0900

    test: core and fix data_holder

--- test/test_core.rb
+++ test/test_core.rb
@@ -1,7 +1,37 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require_relative 'helper/config_define'
+
 include RoughDiary
+
 class Core::Test < Minitest::Test
+  def setup
+    @core = Core.new
+  end
+
+
+  def test_write
+    mock_writer = Minitest::Mock.new
+    mock_writer.expect :write, true
+
+    Writer.stub :new, mock_writer do
+      @core.write
+    end
+
+    mock_writer.verify
+  end
+
+
+  def test_read
+    mock_reader = Minitest::Mock.new
+    mock_reader.expect :read, true
+
+    Reader.stub :new, mock_reader do
+      @core.read
+    end
+
+    mock_reader.verify
+  end
 
 end
--- test/test_data_holder.rb
+++ test/test_data_holder.rb
@@ -42,18 +42,18 @@ class DataHolder::Fix::Test < Minitest::Test
     assert_nil @holder.get(:id)
     assert_instance_of Time, @holder.get(:create_date)
     assert_equal 1, @holder.get(:fix_diary_id)
-    assert_nil @holder.get(:edit_content)
+    assert_nil @holder.get(:edit_diffs)
   end
 
 
   def test_database_format
-    # TODO: rewrite edit_content as actual data format(yaml)
-    @holder.data_edit_content = 'test example'
+    # TODO: rewrite edit_diffs as actual data format
+    @holder.data_edit_diffs = 'test example'
 
     data = @holder.database_format
     assert_match $create_date_regexp, data.create_date
     assert_equal 1, data.fix_diary_id
-    assert_equal 'test example', data.edit_content
+    assert_equal 'test example', data.edit_diffs
   end
 end
 
