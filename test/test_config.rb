949666799c7affa702fa50cf9c88a52c656d0150
aozorarb <aozorarb@icloud.com>
2025-02-15 21:10:52 +0900

    test: new content_generator, editor

--- test/test_config.rb
+++ test/test_config.rb
@@ -9,15 +9,4 @@ class Config::Test< Minitest::Test
     end
   end
 
-
-  def test_need_follow_diary_type
-    res =
-      NEED_FOLLOW_DIARY_TYPE.all? do |need_type_pair|
-        need_type = need_type_pair[0]
-        VALID_DIARY_TYPE.include?(need_type)
-      end
-
-    assert(res)
-  end
-
 end
--- test/test_content_generator.rb
+++ test/test_content_generator.rb
@@ -6,61 +6,28 @@ include RoughDiary
 
 class ContentGenerator::Test < Minitest::Test
   def setup
-    @data_holder = DataHolder
+    @data_holder = DataHolder.new
     @mock_tempfile = Minitest::Mock.new
+    @mock_tempfile.expect :write, nil, [nil]
     @mock_tempfile.expect :close, true
-
+    @mock_editor = Minitest::Mock.new
     Tempfile.stub :create, @mock_tempfile do
-      @generator = ContentGenerator.new(@data_holder)
+      @generator = ContentGenerator.new(@data_holder, @mock_editor)
     end
-
   end
 
 
   def test_run
-    @mock_tempfile.expect :read, 'mock diary content'
+    @mock_editor.expect :edit, true, [Minitest::Mock]
+    @mock_editor.expect :diary_title, 'title'
     @mock_tempfile.expect :reopen, true, ['path/to', 'r']
     @mock_tempfile.expect :path, 'path/to'
-    @data_holder.expect :data_content=, nil, ['mock diary content']
+    @mock_tempfile.expect :read, 'mock diary content'
 
-    @generator.stub :edit_tempfile, nil do
-      @generator.stub :ask_diary_title, true do
-        @generator.run
-      end
-    end
+    @generator.run
 
+    @mock_editor.verify
     @mock_tempfile.verify
-  end
-
-
-  def suppress_output
-    stdout = $stdout
-    $stdout = File.open(File::NULL, 'w')
-    yield
-  ensure
-    $stdout = stdout
-  end
-
-
-  def test_ask_diary_title
-    suppress_output do
-      @data_holder.expect :data_title=, nil, ['test title']
-
-      @generator.stub :gets, "test title\n" do
-        @generator.send(:ask_diary_title)
-      end
-
-    end
-  end
-
-
-  def test_ask_diary_title_is_none
-    suppress_output do
-      @generator.stub :gets, "\n" do
-        @generator.send(:ask_diary_title)
-      end
-
-    end
   end
 
 end
