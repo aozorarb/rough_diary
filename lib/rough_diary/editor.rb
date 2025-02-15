54de1bf9cd8c06e10826eb56eb7e3a49f8020f77
aozorarb <aozorarb@icloud.com>
2025-02-15 23:20:23 +0900

    feat: create SimpleCli

--- lib/rough_diary/editor.rb
+++ lib/rough_diary/editor.rb
@@ -4,7 +4,6 @@ require_relative 'error'
 module RoughDiary
   class Editor
     class Base
-      
       def initialize(data_holder)
         @data_holder = data_holder
 
@@ -21,29 +20,6 @@ module RoughDiary
         RoughDiary::Config::DEFAULT_DIARY_TITLE
       end
     end
-
-
-
-    class SimpleCli < Base
-      def diary_title
-        puts "Please enter diary title (if none, title will be '#{Config::DEFAULT_DIARY_TITLE}')"
-        print 'Diary title: '
-        title = gets.chomp
-
-        title.empty? ? title : Config::DEFAULT_DIARY_TITLE
-      end
-
-
-      def edit(file)
-        path = file.path
-        if DiaryUtils.valid_editor?(Config::EDITOR)
-          `#{Config::EDITOR} #{path}`
-        else
-          raise InvalidConfigrationError, 'Invalid editor: #{Config::EDITOR}'
-        end
-      end
-    end
   end
-
 end
 
---
+++ lib/simple_cli/core.rb
@@ -0,0 +1,28 @@
+require 'rough_diary'
+
+class SimpleCli
+  class Core
+
+  end
+
+  class Editor < RoughDiary::Editor::Base
+    def diary_title
+      puts "Please enter diary title (if none, title will be '#{Config::DEFAULT_DIARY_TITLE}')"
+      print 'Diary title: '
+      title = gets.chomp
+
+      title.empty? ? title : Config::DEFAULT_DIARY_TITLE
+    end
+
+
+    def edit(file)
+      path = file.path
+      if DiaryUtils.valid_editor?(Config::EDITOR)
+        `#{Config::EDITOR} #{path}`
+      else
+        raise InvalidConfigrationError, 'Invalid editor: #{Config::EDITOR}'
+      end
+    end
+  end
+end
+
