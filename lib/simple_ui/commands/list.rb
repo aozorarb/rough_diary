9a9263c99feb69cda74ad0b6f2f3f3ea630e6f30
aozorarb <aozorarb@icloud.com>
2025-03-10 16:52:45 +0900

    feat: improve list view

--- lib/simple_ui/commands/list.rb
+++ lib/simple_ui/commands/list.rb
@@ -10,14 +10,27 @@ class SimpleUi::Commands::List < SimpleUi::Command
   end
 
 
+  private def truncate_text(str, max_size: 50)
+    raise ArgumentError, 'max_size must be positive' if max_size.negative?
+    res = str.dup
+    if str.size > max_size
+      str_size = [str.size, max_size].min
+      res[str_size-3..] = '...'
+    end
+    res
+  end
+
   def execute
     limit = @options[:limit] || 10
     order_by = @options[:order_by] || 'create_date'
 
     db_manager = RoughDiary::DatabaseManager.new(configatron.system.database_path)
     diaries = db_manager.execute("SELECT * FROM diary_entries ORDER BY #{order_by} LIMIT #{limit}")
+    puts '  id | title'
     diaries.each do |diary|
-      str = "#{diary['title']} (#{diary['id']})"
+      id = diary['id'].to_i
+      title = truncate_text(diary['title'])
+      str = format("% 4d | %s", id, title)
       puts str
     end
   end
